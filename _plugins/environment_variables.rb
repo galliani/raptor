e65a1408127edf608b1699242e3ed4baf5c14f96
Galih Muhammad <galih0muhammad@gmail.com>
2018-05-14 22:02:20 +0700

    Add basic repo

---
+++ _plugins/environment_variables.rb
@@ -0,0 +1,22 @@
+# Plugin to add environment variables to the `site` object in Liquid templates
+require 'dotenv'
+
+module Jekyll
+  class EnvironmentVariablesGenerator < Generator
+    priority :highest
+    
+    def generate(site)
+      site.config['env'] = Dotenv.load
+
+      site.config['title'] = ENV['SITE_TITLE']
+      site.config['description'] = ENV['SITE_DESCRIPTION']
+      site.config['production_url'] = ENV['PRODUCTION_URL']
+      site.config['source_url'] = ENV['SOURCE_URL']
+
+      # sensitives
+      site.config['google_analytics'] = ENV['GOOG_ANALYTICS_ID']
+      site.config['twitter'] = ENV['TWTR_HANDLER'] # replace by your username
+      site.config['facebook'] = ENV['FB_USERNAME'] # replace by your username      
+    end
+  end
+end
