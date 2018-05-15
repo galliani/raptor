367eb869702ad8b2d90bb9f53cebb9fa369d8420
Galih Muhammad <galih0muhammad@gmail.com>
2018-05-15 20:21:10 +0700

    Add logos and remove unused images

--- _plugins/environment_variables.rb
+++ _plugins/environment_variables.rb
@@ -8,15 +8,15 @@ module Jekyll
     def generate(site)
       site.config['env'] = Dotenv.load
 
-      site.config['title'] = ENV['SITE_TITLE']
-      site.config['description'] = ENV['SITE_DESCRIPTION']
+      site.config['title']          = ENV['SITE_TITLE']
+      site.config['description']    = ENV['SITE_DESCRIPTION']
       site.config['production_url'] = ENV['PRODUCTION_URL']
-      site.config['source_url'] = ENV['SOURCE_URL']
+      site.config['source_url']     = ENV['SOURCE_URL']
 
       # sensitives
       site.config['google_analytics'] = ENV['GOOG_ANALYTICS_ID']
-      site.config['twitter'] = ENV['TWTR_HANDLER'] # replace by your username
-      site.config['facebook'] = ENV['FB_USERNAME'] # replace by your username      
+      site.config['twitter']          = ENV['TWTR_HANDLER'] # replace by your username
+      site.config['facebook']         = ENV['FB_USERNAME'] # replace by your username      
     end
   end
 end
